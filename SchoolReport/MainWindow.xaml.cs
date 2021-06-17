75de3588208d3cc0e9c61fff12ec6d1ac4d38f64
tachoco <vanilla.airi@gmail.com>
2021-06-17 09:07:43 +0900

    タブにある複数のテーブルを編集するためのプロジェクト

---
+++ SchoolReport/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolReport
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ScoreTable score = new ScoreTable()
+            {
+                Id = 1,
+                Name = "Tanaka",
+                Japanese = 80,
+                Math = 80,
+                English = 80,
+                Science = 80,
+                Social = 80,
+            };
+        }
+    }
+}
---
+++ SchoolReport/ScoreTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolReport
+{
+    public class ScoreTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Japanese { get; set; }
+        public int Math { get; set; }
+        public int English { get; set; }
+        public int Science { get; set; }
+        public int Social { get; set; }
+    }
+}
