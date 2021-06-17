06cfe36bfc8b3fd0ec630fdce4beaec4cb1e8f22
tachoco <vanilla.airi@gmail.com>
2021-06-17 09:07:14 +0900

    MVVMを使ってDataGridの行を追加・削除

---
+++ MvvmEditTable/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MvvmEditTable
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public RowEditCommand rowEditCommand { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public TableRows tableRows;
+        private ObservableCollection<TableRow> tableData = new ObservableCollection<TableRow>();
+
+        public ObservableCollection<TableRow> TableData {
+            get { return tableData; }
+            set 
+            {
+                OnPropertyChanged(nameof(TableData));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            rowEditCommand = new RowEditCommand(this);
+
+            tableRows = new TableRows();
+            tableData = tableRows.InitializeTable();
+        }
+
+        public void OnPropertyChanged(string parameter = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(parameter));
+        }
+    }
+}
---
+++ MvvmEditTable/TableRows.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmEditTable
+{
+    public class TableRow
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class TableRows
+    {
+        public TableRows()
+        {
+        }
+
+        public ObservableCollection<TableRow> InitializeTable()
+        {
+            ObservableCollection<TableRow> tableData = new ObservableCollection<TableRow>()
+            {
+                new TableRow(){ Id = 1, Name = "Tanaka", Age = 20 },
+                new TableRow(){ Id = 2, Name = "Sato", Age = 21},
+                new TableRow(){ Id = 3, Name = "Suzuki", Age = 22}
+            };
+
+            return tableData;
+        }
+    }
+}
