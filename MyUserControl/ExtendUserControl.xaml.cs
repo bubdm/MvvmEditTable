0a9b933a0c3cc4795e01dbec9f81814187688862
tachoco <vanilla.airi@gmail.com>
2021-06-21 18:41:51 +0900

    自分で作り直そうとしてうまくいかなかったもの

---
+++ MyUserControl/ExtendUserControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyUserControl
+{
+    /// <summary>
+    /// ExtendUserControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ExtendUserControl : UserControl
+    {
+        public int Id { get; set; } = -1;
+        public ExtendUserControl()
+        {
+            InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+    }
+}
---
+++ MyUserControl/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MyUserControl
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string input = "DefaultText";
+        public string Input
+        {
+            get { return input; }
+            set
+            {
+                input = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Input)));
+            }
+        }
+    }
+}
