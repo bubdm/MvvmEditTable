9e1eff91847344062a51bc98ebb87541fa325b47
tachoco <vanilla.airi@gmail.com>
2021-06-21 09:48:28 +0900

    Bindingの練習をするサンプル

---
+++ BindingSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingSample
+{
+    public class MainWindowViewModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Name = "Sakai";
+            Address = "Toyko";
+        }
+    }
+}
