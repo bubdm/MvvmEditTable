7048b42e70d43f64c5439855f19f621435c3f13e
tachoco <vanilla.airi@gmail.com>
2021-06-21 15:46:09 +0900

    https://blog.okazuki.jp/entry/2014/09/08/203943 RepeatButtonで数字を増減させるサンプル

---
+++ NumericUpDownSample/NumericUpDown.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumericUpDownSample
+{
+    /// <summary>
+    /// NumericUpDown.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            "Value",
+            typeof(int),
+            typeof(NumericUpDown),
+            new PropertyMetadata(0, ValueChanged)
+        );
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private static void ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((NumericUpDown)d).UpdateState(true);
+        }
+
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value++;
+            this.UpdateState(false);
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value--;
+        }
+
+        private void UpdateState(bool useTransition)
+        {
+            if (this.Value >= 0)
+            {
+                VisualStateManager.GoToState(this, "Positive", useTransition); 
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Negative", useTransition);
+            }
+        }
+    }
+}
